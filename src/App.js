0fa9495bf0835db4acadcfda3866c6e25371342b
k0m0ri <komori.sc14@gmail.com>
2023-09-30 22:44:59 +0900

    プロフィールページを作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
-    <Home />
+    <Profile />
   );
 }
 
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,21 +3,21 @@ import Online from '../online/Online'
 import { Users } from '../../dummyData'
 import './Rightbar.css'
 
-export default function Rightbar() {
-  return (
-    <div className="rightbar">
-        <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+    const HomeRightbar = () => {
+        return (
+            <>
             <div className="eventContainer">
                 <img src="/assets/star.png" alt="" className='starImg'/>
                 <span className='eventText'>
-                    <b>フォロワー限定</b>イベント開催中！
-                </span>
+                <b>フォロワー限定</b>イベント開催中！
+            </span>
             </div>
             <img src="/assets/event.jpeg" alt="" className='eventImg'/>
             <h4 className='rightbarTitle'>オンラインの友達</h4>
             <ui className="rightbarFriendList">
                 {Users.map((user) => (
-                    <Online user={user} key={user.id} />
+                <Online user={user} key={user.id} />
                 ))}
             </ui>
             <p className='promotionTitle'>プロモーション広告</p>
@@ -27,7 +27,44 @@ export default function Rightbar() {
             <p className='promotionName'>カーショップ</p>
             <img src="/assets/promotion/promotion3.jpeg" alt="" className='rightbarPromotionImg'/>
             <p className='promotionName'>クリエイティブ株式会社</p>
+            </>
+        )
+    }
+
+    const ProfileRightbar = () => {
+        return (
+            <>
+            <h4 className="rightbarTitle">ユーザー情報</h4>
+            <div className="rightbarInfo">
+                <div className="rightbarInfoItem">
+                    <span className='rightbarInfoKey'>出身：</span>
+                    <span className='rightbarInfoKey'>福岡</span>
+                </div>
+            </div>
+            <h4 className="rightbarTitle">あなたの友達</h4>
+            <div className="rightbarFollowings">
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className='rightbarFollowingName'>tarou</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className='rightbarFollowingName'>tanaka</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className='rightbarFollowingName'>yamada</span>
+                </div>
+            </div>
+            </>
+        )
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                {profile ? <ProfileRightbar /> : <HomeRightbar />}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
