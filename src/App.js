4cf5a6f0d17255d953779c3bf6fb969ed5ba2136
k0m0ri <komori.sc14@gmail.com>
2023-09-30 22:56:28 +0900

    ログインと新規登録画面を作成

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
-    <Profile />
+    <Home />
   );
 }
 
